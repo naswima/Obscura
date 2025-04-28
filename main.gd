ce2ce1c0e0fcb1808994526cfc05e72e4fcc0414
aniya <fyeasmin1@fordham.edu>
2025-04-28 16:16:53 -0400

    buttons

--- main.gd
+++ main.gd
@@ -13,11 +13,12 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://level1.tscn")
+	pass
 
 
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://howto.tscn")
-
+	get_tree().change_scene_to_file("res://how to play.tscn")
+	
 
 func respawn():
 	get_tree().reload_current_scene()
