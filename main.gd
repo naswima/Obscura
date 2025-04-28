e70a98f8a01ad2e689b4402a0e987035375f8a1f
kristine1233 <kpak6@fordham.edu>
2025-04-28 15:27:39 -0400

    Respawn

--- main.gd
+++ main.gd
@@ -17,3 +17,9 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://howto.tscn")
+
+
+func respawn():
+	get_tree().reload_current_scene()
+	
+	
