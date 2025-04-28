6b197e5b54694d69c34412ca72809454805d491c
naswima <nakter5@fordham.edu>
2025-04-28 15:25:28 -0400

    Revert "updates"
    This reverts commit c2dbc8f16611fca6572252aed8126f6b0b7172df, reversing
    changes made to b2af93485cfef689104d7210cbbe76c0bd0b7cf1.

--- main.gd
+++ main.gd
@@ -13,7 +13,5 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://level1.tscn")
-
-
-func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://howto.tscn")
+	
+	pass # Replace with function body.
