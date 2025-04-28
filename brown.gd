e6a3520fcd125c4b86c7edaaa98e1de670aa0744
naswima <nakter5@fordham.edu>
2025-04-28 15:37:10 -0400

    updates

--- brown.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-var texture = ImageTexture.new()
-var image = Image.load_from_file("res://your_texture.png")
-texture.create_from_image(image)
-texture.flags |= Texture.FLAG_REPEAT
