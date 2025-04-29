b03e167b4410a5767c3b150e995eb9ebe0348f9b
naswima <nakter5@fordham.edu>
2025-04-28 23:56:15 -0400

    Portal & Checkpoint for lvl 1

--- hamster.gd
+++ hamster.gd
@@ -49,3 +49,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	pass # Replace with function body.
+
+
+func _on_portal_body_entered(body: Node2D) -> void:
+	if body == self:
+		get_tree().change_scene_to_file("res://level 2.tscn")
