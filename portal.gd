3e78b4a75c97653c92611e7c71e0989c328cbfad
naswima <nakter5@fordham.edu>
2025-05-05 13:01:32 -0400

    fixed portal code, and text

--- portal.gd
+++ portal.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
-@export var fruitneeded = 10 
+@export var fruitneeded = 30 
 @export var nextlevel : PackedScene
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if body.pickedupitems == fruitneeded:
-			get_tree().change_scene_to_packed(nextlevel)
+			if nextlevel is PackedScene:
+				get_tree().change_scene_to_packed(nextlevel)
+			else:
+				push_error("nextlevel is not a valid PackedScene!")
+		else:
+			print("Uh oh! 🍓 You need all the fruits before you can go! Looks like you're missing a few—go grab 'em and try again!")
