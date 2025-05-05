f0497565447429e854afed7829f44c7bc62a1a0a
naswima <nakter5@fordham.edu>
2025-05-05 13:37:59 -0400

    back button

--- level_1.gd
+++ level_1.gd
@@ -46,3 +46,8 @@ func _on_area_2d_5_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_5_body_exited(body: Node2D) -> void:
 	player_entered = false
+
+
+func _on_button_pressed() -> void:
+	var main_scene = load("res://main.tscn")
+	get_tree().change_scene_to_packed(main_scene)
