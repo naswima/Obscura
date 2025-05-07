640ef26d4a3e2bb627982eac9bf29b034d7558af
kristine1233 <kpak6@fordham.edu>
2025-05-07 19:49:25 -0400

    Code

--- portal.gd
+++ portal.gd
@@ -7,7 +7,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if body.pickedupitems == fruitneeded:
-			if nextlevel is PackedScene:
+			if nextlevel:
 				get_tree().change_scene_to_packed(nextlevel)
 			else:
 				push_error("nextlevel is not a valid PackedScene!")
