528656773393ef953cc750e71d2c0a21bd03cad4
kristine1233 <kpak6@fordham.edu>
2025-05-07 23:13:16 -0400

    Code

--- items.gd
+++ items.gd
@@ -4,9 +4,9 @@ extends Node2D
 
 func _ready() -> void:
 	$AnimatedSprite2D.play(fruitname)
-	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"): 
-		body.pickedupitems += 1
-		queue_free()
+	if body.is_in_group("player"):
+		if body.has_method("pickedupitems"):  
+			body.pickedupitems += 1
+			queue_free()
