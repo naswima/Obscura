af19dbd905e0d18eb72c63303b03418a1af88013
naswima <nakter5@fordham.edu>
2025-05-07 22:11:14 -0400

    updates

--- portal.gd
+++ portal.gd
@@ -28,5 +28,6 @@ func _on_body_entered(body: Node2D) -> void:
 				push_error("nextlevel is not a valid PackedScene!")
 		else:
 			$MissingFruitsPopup.popup_centered()
+
 func _on_ok_button_pressed():
 	$MissingFruitsPopup.hide()
