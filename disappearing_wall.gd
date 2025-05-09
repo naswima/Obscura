59676e8f0b4b29c7e4c365f1b039aa0cec3201ba
kristine1233 <kpak6@fordham.edu>
2025-05-09 09:24:23 -0400

    Code for popup

--- disappearing_wall.gd
+++ disappearing_wall.gd
@@ -5,17 +5,18 @@ extends Area2D
 @onready var player: CharacterBody2D = get_node_or_null("/root/level2/mr_frog")
 @onready var missing_fruits_popup: Popup = $MissingFruitsPopup
 
-@export var fruitneeded := 2
+@export var fruitneeded := 50
 
 func _ready():
 	missing_fruits_popup.hide()
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _process(delta: float) -> void:
-	if player and player.pickedupitems >= 2:
+	if player and player.pickedupitems >= 50:
 		queue_free()
 
 func _on_body_entered(body):
+	print("Body entered:", body)
 	if body.is_in_group("player"):
 		if body.pickedupitems < fruitneeded:
 			missing_fruits_popup.popup_centered()
